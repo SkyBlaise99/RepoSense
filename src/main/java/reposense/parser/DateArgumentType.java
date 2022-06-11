2516996dcda46f5df695198b021217fa2770af9e
SkyBlaise <zsc990124@gmail.com>
2022-06-11 12:40:07 +0800

    Update help message

--- src/main/java/reposense/parser/DateArgumentType.java
+++ src/main/java/reposense/parser/DateArgumentType.java
@@ -13,8 +13,10 @@ import reposense.util.TimeUtil;
  * Verifies and parses a string-formatted date to a {@link LocalDateTime} object.
  */
 public class DateArgumentType implements ArgumentType<Optional<LocalDateTime>> {
-    protected static final String EXTRACT_EXCEPTION_MESSAGE_INVALID_DATE_FORMAT =
-            "Invalid Date Format: %s. Please read usage guide above.";
+    protected static final String EXTRACT_EXCEPTION_MESSAGE_INVALID_DATE_FORMAT = "Invalid Date: %s\n"
+            + "Year must be within 1900 - 2999.\n"
+            + "Accepted formats: [d]d/[M]M/yyyy. For example: 01/01/2022 or 1/1/2022.\n"
+            + "Accepted delimiters: '/', '-', '.'. For example: 1/1/2022 or 1-1-2022.";
     private static final String PARSE_EXCEPTION_MESSAGE_INVALID_DATE_STRING_FORMAT = "Invalid Date: %s";
 
     @Override
